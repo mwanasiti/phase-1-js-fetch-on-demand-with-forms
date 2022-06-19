a1ac0b48946d46dfd27dc12d2c0f6a97ae75f51c
Mwanasiti <ali.mariam.mwanasiti@gmail.com>
2022-06-19 14:39:48 +0300

    forms

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-const init = () => {
+// const init = () => {
   
+// }
+
+// document.addEventListener('DOMContentLoaded', init);
+
+
+let url="http://localhost:3000/movies"
+document.addEventListener('DOMContentLoaded', ()=>{
+    getFetch()
+    formData()
+})
+
+function getFetch(){
+    fetch(url).then(resp=>resp.json()).then(mariam=>console.log(mariam))
 }
 
-document.addEventListener('DOMContentLoaded', init);
+function formData(){
+    const form=document.querySelector("form")
+    form.addEventListener('submit', ()=>{
+        
+    })
+}
